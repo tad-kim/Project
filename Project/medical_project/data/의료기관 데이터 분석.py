ab912d65394891c041c578a4c0db5ba2e1ea4323
tad <zkdlto@naver.com>
2025-07-23 20:43:05 +0900

    영업상태 확인

--- Project/medical_project/data/의료기관 데이터 분석.py
+++ Project/medical_project/data/의료기관 데이터 분석.py
@@ -17,5 +17,20 @@ medical = pd.read_csv("C:\\Users\\zkdlt\\Documents\\GitHub\\Project\\medical_pro
 #print(health)
 print(medical.columns)
 
+# 미션 1. 전국 병원의 영업상태(정상/폐업/휴업) 분포를 분석 도출
+
+# 우선 영업상태명 컬럼으로 정상,폐업,휴업 등 영업상태를 확인
+medical_ = medical['영업상태명'].value_counts(normalize=True)
+
+medical_df = medical_ * 100
+print(medical_df)
+
+plt.rc('font', family='Malgun Gothic')
+plt.figure(figsize=(12, 6))
+medical_df.plot(kind='pie', autopct='%.1f%%', startangle=90)
+plt.title("전국 의료기관 영업상태")
+plt.ylabel("") # 원형 그래프에선 y축 라벨 필요없음
+plt.tight_layout()
+plt.show()
 
 
